68a1bab29c6d94c4214bf699c88f4111047ac907
Adam Jurczyk <adam.jurczyk@grupaonet.pl>
2014-09-17 00:46:54 +0200

    ls tweaks

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -91,10 +91,11 @@ else
 fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
+alias la='ls -a'         # Lists hidden files
 alias ll='ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+alias lla='ll -A'        # Lists human readable sizes, hidden files.
+alias lm='lla | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
