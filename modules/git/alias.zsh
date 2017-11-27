581bcffa04b5fee61f1a3ea828c494a71a1e9748
Adam Jurczyk <ajur.pl@gmail.com>
2017-11-27 16:39:05 +0100

    git aliases tuning

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -14,7 +14,7 @@
 zstyle -s ':prezto:module:git:log:medium' format '_git_log_medium_format' \
   || _git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
 zstyle -s ':prezto:module:git:log:oneline' format '_git_log_oneline_format' \
-  || _git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
+  || _git_log_oneline_format='%C(green)%h%C(reset) %s%C(blue)%d%C(reset) %C(dim)%an %ar%C(reset)'
 zstyle -s ':prezto:module:git:log:brief' format '_git_log_brief_format' \
   || _git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
 
@@ -101,7 +101,8 @@ alias gfr='git pull --rebase'
 
 # Log (l)
 #alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
-alias gl='git log --graph --all --date-order --pretty=format:"%C(green)%h%C(reset) %s%C(blue)%d%C(reset) %C(black)%an %ar%C(reset)"'
+alias gl='git log --graph --all --date-order --pretty=format:"${_git_log_oneline_format}"'
+alias glb='git log --graph --date-order --pretty=format:"${_git_log_oneline_format}"'
 alias glmd='git log --grep="Merge pull request" master..develop --pretty=format:"%s" | cut -d\  -f 8 | sort -u'
 # alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
 # alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
@@ -179,5 +180,5 @@ alias gwr='git reset --soft'
 alias gwR='git reset --hard'
 alias gwc='git clean -n'
 alias gwC='git clean -f'
-alias gwx='git rm -r'
-alias gwX='git rm -rf'
+# alias gwx='git rm -r'
+# alias gwX='git rm -rf'
